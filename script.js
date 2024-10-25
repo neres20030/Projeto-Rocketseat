b59ce21d02de660ea0e97f60f3e64a6e9f5eb657
João Paulo <joaopaulo.lealneres@gmail.com>
2024-10-25 15:52:50 -0300

    nova alteração

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function toggleMode() {
     const html = document.documentElement;
     html.classList.toggle("light");
 
-    const img = document.querySelector("#profile img"); // Adicione o "#" para selecionar o ID corretamente
+    const img = document.querySelector("#profile img"); 
     if (html.classList.contains("light")) {
         img.setAttribute("src", "./assets/avatar-light.png");
     } else {
